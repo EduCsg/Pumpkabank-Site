30d9d5a9467b78489183e686a6999e8cbc7c4ce9
Eduardo <eduardocasagrande71@gmail.com>
2023-06-22 18:59:44 -0300

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
 	return (
 		<>
-			<div className="h-[100vh] flex items-center justify-center">
-				<h1 className="text-8xl text-purple text-white font-bold">
-					Pumpkabank!
-				</h1>
-			</div>
+			<Header />
 		</>
 	);
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+const Header = () => {
+	return (
+		<header className="flex fixed w-[80vw] left-1/2 -translate-x-1/2 justify-between items-center">
+			<img
+				className="h-[90px] w-[90px]"
+				src="src/assets/logo.svg"
+				alt="Logo do Pumpkabank"
+			/>
+
+			<nav className="bg-white rounded-b-[50px] h-24 flex px-16 gap-24 items-center font-semibold text-purple">
+				<ul className="flex gap-4">
+					<li className="p-4 rounded-xl hover:bg-pink hover:cursor-pointer hover:text-white transition-all duration-200 ease-out">
+						<a href="#">Lojas</a>
+					</li>
+					<li className="p-4 rounded-xl hover:bg-pink hover:cursor-pointer hover:text-white transition-all duration-200 ease-out">
+						<a href="#">Parceiros</a>
+					</li>
+					<li className="p-4 rounded-xl hover:bg-pink hover:cursor-pointer hover:text-white transition-all duration-200 ease-out">
+						<a href="#">Saiba mais</a>
+					</li>
+				</ul>
+
+				<ul className="group flex gap-4">
+					<li>
+						<a
+							href="#"
+							className="p-4 rounded-xl hover:bg-pink hover:cursor-pointer hover:text-white transition-all duration-200 ease-out"
+						>
+							Criar conta
+						</a>
+					</li>
+
+					<li>
+						<a
+							href="#"
+							className="p-4 rounded-xl bg-pink text-white hover:cursor-pointer group-hover:text-purple group-hover:bg-white ring-pink hover:!text-pink hover:ring-2 ring-inset transition-all duration-200 ease-out"
+						>
+							Login
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
