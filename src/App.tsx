b667d9d007cd33d61d69213de83bc64e1f140476
Eduardo <eduardocasagrande71@gmail.com>
2023-06-22 16:47:32 -0300

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-	return <h1>Pumpkabank!</h1>;
+	return (
+		<>
+			<div className="h-[100vh] flex items-center justify-center">
+				<h1 className="text-8xl text-purple text-white font-bold">
+					Pumpkabank!
+				</h1>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				purple: "#320995",
+				lpurple: "#3b0cae",
+				pink: "#e23063",
+				green: "#26E55F",
+			},
+		},
+	},
+	plugins: [],
+};
