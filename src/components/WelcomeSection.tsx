c93bf9cb7e66bc09a494476e958059d5adb812d9
Eduardo <eduardocasagrande71@gmail.com>
2023-06-22 23:53:56 -0300

    Fix downloaded ColorIcons and create button

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -2,20 +2,27 @@ import welcomeImage from "../assets/Welcome.png";
 
 import divider01 from "../assets/dividers/Divider01.svg";
 
+import ColorIcon1 from "../assets/colorsIcons/Color1.svg";
+import ColorIcon2 from "../assets/colorsIcons/Color2.svg";
+import ColorIcon3 from "../assets/colorsIcons/Color3.svg";
+import ColorIcon4 from "../assets/colorsIcons/Color4.svg";
+import ColorIcon5 from "../assets/colorsIcons/Color5.svg";
+import ColorIcon6 from "../assets/colorsIcons/Color6.svg";
+
 const WelcomeSection = () => {
 	const ColorsIcons = [
-		"Color1.svg",
-		"Color2.svg",
-		"Color3.svg",
-		"Color4.svg",
-		"Color5.svg",
-		"Color6.svg",
+		ColorIcon1,
+		ColorIcon2,
+		ColorIcon3,
+		ColorIcon4,
+		ColorIcon5,
+		ColorIcon6,
 	];
 
 	return (
 		<section className="flex flex-col">
-			<div className="flex items-center font-semibold max-w-[80vw] mx-auto mt-16">
-				<div className="flex flex-col gap-4 w-[50vw]">
+			<div className="flex items-center font-semibold max-w-[80vw] mx-auto mt-12">
+				<div className="flex flex-col gap-5 w-[50vw]">
 					<h1 className="text-[60px] max-w-[5w] text-white">
 						O 1º banco que conecta
 						<br />
@@ -26,15 +33,25 @@ const WelcomeSection = () => {
 						De Kanto a Paldea, seu dinheiro onde você quiser!
 					</p>
 
-					<div className="flex gap-1">
+					<div className="flex gap-3">
 						{ColorsIcons &&
 							ColorsIcons.map((color, index) => (
 								<img
-									src={`src/assets/colorsIcons/${color}`}
+									src={color}
 									key={index}
-									className={`w-[50px] h-[50px] rounded-full bg-${color} `}
+									className={
+										index == 0
+											? ` ring-offset-purple ring-offset-4 ring-pink ring-2 rounded-full`
+											: ``
+									}
 								/>
 							))}
+					</div>
+
+					<div>
+						<button className="bg-pink px-12 py-5 text-white rounded-lg">
+							Criar Conta
+						</button>
 					</div>
 				</div>
 
